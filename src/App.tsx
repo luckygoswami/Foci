c583bace028e8f42f86c70129835e2975406746d
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-05-13 23:50:08 +0530

    feat: replace Inter font with Poppins, created AuthForm component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,7 @@
-import { toast, ToastContainer } from 'react-toastify';
-import { Button } from './components/ui/button';
+import AuthForm from './features/auth/components/AuthForm';
 
 function App() {
-  return (
-    <>
-      <div className="flex justify-between items-center h-screen">
-        <p className=" text-xl p-30 font-extralight">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet beatae
-          expedita molestiae ullam minus ipsa officia accusantium! Commodi
-          facere blanditiis harum tempora, officiis cumque nobis provident
-          libero nemo iure aliquam? Maiores eos rerum provident, quod magnam qui
-          praesentium ipsum, temporibus dolorem saepe ullam reprehenderit
-          necessitatibus ipsam laboriosam ducimus consectetur quasi error
-          tempora quae sint, accusamus hic dolores ipsa. Quaerat, corrupti!
-          Doloremque perspiciatis aperiam maxime aliquam quae. Cumque, odit.
-          Veritatis, laudantium inventore quam nihil dolore iure? Nesciunt
-          aspernatur beatae inventore molestias consequatur tempora dolor odit
-          error quas! Magni fugiat illum similique.
-        </p>
-        <p className="m-30 text-xl jetBrains-text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
-          adipisci unde voluptates velit esse ea modi, perspiciatis dolores
-          numquam necessitatibus vero eveniet recusandae quibusdam atque rem
-          dolore. Fuga, vero blanditiis? Odio, explicabo nihil porro voluptatem
-          deleniti ullam id sunt incidunt, provident consectetur atque veritatis
-          molestiae! Tempora odio nesciunt totam excepturi odit praesentium ab,
-          amet debitis mollitia impedit, suscipit aspernatur ipsam. Aut
-          accusamus sunt illo optio hic earum accusantium distinctio, inventore
-          sapiente maxime molestias ex eos. Accusamus iste, maiores odit tenetur
-          aut sapiente, reiciendis esse illo assumenda id temporibus tempora
-          qui?
-        </p>
-        <Button onClick={() => toast.success('too easy!')}>
-          toast the msg
-        </Button>
-        <ToastContainer
-          position="top-center"
-          toastStyle={{
-            backgroundColor: 'var(--background)',
-          }}
-        />
-      </div>
-    </>
-  );
+  return <AuthForm />;
 }
 
 export default App;
---
+++ src/features/auth/components/AuthForm.tsx
@@ -0,0 +1,135 @@
+import { useRef } from 'react';
+import './AuthForm.css';
+
+function LoginForm() {
+  const container = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="body">
+      <div
+        className="container"
+        ref={container}>
+        <div className="form-box login">
+          <form action="">
+            <h1 className="font-bold">Login</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Username"
+                required
+              />
+              <i className="fa-solid fa-user"></i>
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Enter Password"
+                required
+              />
+              <i className="fa-solid fa-lock"></i>
+            </div>
+            <div className="forgot-link">
+              <a href="#">Forgot Password</a>
+            </div>
+            <button
+              type="submit"
+              className="btn">
+              Login
+            </button>
+            <p>or login with social platforms</p>
+            <div className="social-icons">
+              <a href="#">
+                <i className="fa-brands fa-google"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+        <div className="form-box register">
+          <form action="">
+            <h1 className="font-bold">Registration</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Username"
+                required
+              />
+              <i className="fa-solid fa-user"></i>
+            </div>
+            <div className="input-box">
+              <input
+                type="email"
+                placeholder="Email"
+                required
+              />
+              <i className="fa-solid fa-envelope"></i>
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Enter Password"
+                required
+              />
+              <i className="fa-solid fa-lock"></i>
+            </div>
+            <button
+              type="submit"
+              className="btn">
+              Register
+            </button>
+            <p>or register with social platforms</p>
+            <div className="social-icons">
+              <a href="#">
+                <i className="fa-brands fa-google"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+
+        <div className="toggle-box">
+          <div className="toggle-panel toggle-left">
+            <h1 className="font-bold">Hello, Welcome!</h1>
+            <p>Don't have an account?</p>
+            <button
+              className="btn register-btn"
+              onClick={() => {
+                container.current?.classList.add('active');
+              }}>
+              Register
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1 className="font-bold">Welcome Back!</h1>
+            <p>Already have an account?</p>
+            <button
+              className="btn login-btn"
+              onClick={() => {
+                container.current?.classList.remove('active');
+              }}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
