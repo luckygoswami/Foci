4a83a9b7acf6fe66c6ac8cbea03aaece7feb29e3
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-06-06 22:37:58 +0530

    refactor: reorganize imports and update function names for clarity in studySessions module

--- src/lib/firestore/studySessions.ts
+++ src/lib/firestore/studySessions.ts
@@ -1,19 +1,58 @@
-import { addDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  getDoc,
+  doc,
+} from 'firebase/firestore';
 import { db } from '../firebase-config';
 import type { StudySession } from '@/types/study';
 import type { FirebaseUserId } from '@/types/core';
 
-export const addStudySession = async (session: StudySession): Promise<void> => {
-  await addDoc(collection(db, 'studySessions'), session);
+export const addStudySession = async (
+  sessionData: StudySession
+): Promise<void> => {
+  await addDoc(collection(db, 'studySessions'), sessionData);
+  const userRef = doc(db, 'users', sessionData.userId);
+  const userDoc = await getDoc(userRef);
+
+  if (userDoc.exists()) {
+    const prevTime = userDoc.data().totalStudyTime;
+    await updateDoc(userRef, {
+      totalStudyTime: prevTime + sessionData.duration,
+    });
+  }
 };
 
-export const getUserStudySessions = async (
+export const getStudySessionsByUser = async (
   userId: FirebaseUserId
 ): Promise<StudySession[]> => {
   const q = query(
     collection(db, 'studySessions'),
     where('userId', '==', userId)
   );
+  const snapshot = await getDocs(q);
+  return snapshot.docs.map((doc) => doc.data() as StudySession);
+};
+
+export const getStudySessionsByDate = async (
+  userId: FirebaseUserId,
+  date: string,
+  tillDate?: string
+): Promise<StudySession[]> => {
+  const startOfTheDay = Date.parse(`${date}, 12:00:00 am`);
+  const endOfTheDay = Date.parse(`${tillDate || date}, 11:59:59 pm`);
+
+  const q = query(
+    collection(db, 'studySessions'),
+    where('userId', '==', userId),
+    where('startTime', '>=', startOfTheDay),
+    where('startTime', '<=', endOfTheDay)
+  );
+
   const snapshot = await getDocs(q);
   return snapshot.docs.map((doc) => doc.data() as StudySession);
 };
