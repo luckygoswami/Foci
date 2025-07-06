dfa1f52b274790d8ab4d50be549ba6bad41895ea
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-07-06 15:22:30 +0530

    refactor: remove unused exports from session barrel file

--- src/features/sessions/index.ts
+++ src/features/sessions/index.ts
@@ -1,6 +1,4 @@
-export * from './components/SessionConflictDialog';
 export * from './context/CurrentSessionProvider';
-export * from './hooks/useHydratedSession';
 export * from './services/firestoreSession';
 export * from './services/localSession';
 export * from './services/remoteSession';
