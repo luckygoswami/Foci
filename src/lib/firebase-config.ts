36222ed4675a04d8f87729a1518b5b3bca3d94ee
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-07-18 13:59:02 +0530

    feat: add Firebase configuration and emulator connections for Firestore, Database, and Auth

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,21 +1,36 @@
 import { initializeApp } from 'firebase/app';
 import {
+  connectFirestoreEmulator,
   initializeFirestore,
   persistentLocalCache,
   persistentMultipleTabManager,
 } from 'firebase/firestore';
-import { getDatabase } from 'firebase/database';
-import { getAuth } from 'firebase/auth';
+import { connectDatabaseEmulator, getDatabase } from 'firebase/database';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_MEASUREMENT_ID,
+  VITE_FIREBASE_DATABASE_URL,
+  VITE_FIRESTORE_EMULATOR_HOST,
+  VITE_DATABASE_EMULATOR_HOST,
+  VITE_AUTH_EMULATOR_HOST,
+} = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: VITE_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -28,5 +43,13 @@ const db = initializeFirestore(app, {
 });
 
 const rtdb = getDatabase(app);
+
+if (import.meta.env.DEV) {
+  connectFirestoreEmulator(db, `${VITE_FIRESTORE_EMULATOR_HOST}`, 8080);
+  connectDatabaseEmulator(rtdb, `${VITE_DATABASE_EMULATOR_HOST}`, 9000);
+  connectAuthEmulator(firebaseAuth, `http://${VITE_AUTH_EMULATOR_HOST}:9099`, {
+    disableWarnings: true,
+  });
+}
 
 export { firebaseAuth, db, rtdb };
