a7102aaeade01b136a7af06e2cfea7cdecfce675
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-07-18 12:50:39 +0530

    feat: implement AuthProvider with authentication context and methods for user login, signup, and logout

---
+++ src/features/auth/context/AuthProvider.tsx
@@ -0,0 +1,101 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useMemo,
+  type ReactNode,
+} from 'react';
+import {
+  onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  type User,
+} from 'firebase/auth';
+import { firebaseAuth } from '@/lib/firebase-config';
+
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+  googleLogin: () => Promise<void>;
+  emailLogin: (email: string, password: string) => Promise<void>;
+  emailSignup: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(firebaseAuth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const emailSignup = async (email: string, password: string) => {
+    try {
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.error('Signup failed:', error);
+    }
+  };
+
+  const emailLogin = async (email: string, password: string) => {
+    try {
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  const googleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(firebaseAuth, provider);
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(firebaseAuth);
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+
+  const authContextValue = useMemo(
+    () => ({
+      user,
+      loading,
+      googleLogin,
+      emailLogin,
+      emailSignup,
+      logout,
+    }),
+    [user, loading]
+  );
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
