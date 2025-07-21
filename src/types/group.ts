b0b93001d75bd1bde5e87b08fe2adef2e25184d9
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-07-21 23:30:47 +0530

    feat: update Group interface to include groupId and avatarId, and change joinCode type to string

--- src/types/group.ts
+++ src/types/group.ts
@@ -1,14 +1,16 @@
-import type { FirebaseUserId } from './core';
+import type { FirebaseUserId, GroupId } from './core';
 
 export interface Group {
+  groupId: GroupId;
   name: string;
+  avatarId: string;
   description: string;
   tags?: string[];
   creatorId: FirebaseUserId;
   memberCount: number;
   members: GroupMember[];
   isPublic: boolean;
-  joinCode: number;
+  joinCode: string;
   createdAt: number;
   updatedAt: number;
 }
