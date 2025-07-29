798e7fb5308b8af216e4f4fbbc10cf5299034c8e
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-07-29 23:26:01 +0530

    feat: add FriendsInviteModal for inviting friends and manage group membership with IntersectionObserver to reduce db calls

--- src/features/groups/services/groups.ts
+++ src/features/groups/services/groups.ts
@@ -7,9 +7,20 @@ import {
   query,
   where,
   getDocs,
+  setDoc,
+  deleteDoc,
+  arrayUnion,
+  arrayRemove,
 } from 'firebase/firestore';
 import { db } from '@/lib/firebase-config';
-import type { FirebaseUserId, Group, GroupId, GroupMember } from '@/types';
+import type {
+  FirebaseUserId,
+  Group,
+  GroupId,
+  GroupInvite,
+  GroupMember,
+  UserData,
+} from '@/types';
 import type { GroupMemberRoles } from '../types';
 
 export const fetchGroupById = async (
@@ -20,7 +31,7 @@ export const fetchGroupById = async (
   return snapshot.exists() ? (snapshot.data() as Group) : null;
 };
 
-export const getGroupsJoinedByUser = async (
+export const fetchGroupsJoinedByUser = async (
   userId: FirebaseUserId
 ): Promise<(Group & { groupId: string })[]> => {
   const q = query(
@@ -28,6 +39,8 @@ export const getGroupsJoinedByUser = async (
     where('memberIds', 'array-contains', userId)
   );
   const snapshot = await getDocs(q);
+  if (snapshot.empty) return [];
+
   return snapshot.docs.map((doc) => {
     return { groupId: doc.id, ...(doc.data() as Group) };
   });
@@ -53,6 +66,15 @@ export const updateGroup = async (
   await updateDoc(groupRef, data);
 };
 
+export const deleteGroup = async (groupId: GroupId): Promise<void> => {
+  const groupRef = doc(db, 'groups', groupId);
+  const groupDoc = await getDoc(groupRef);
+
+  if (!groupDoc.exists()) return;
+  // TODO: delete all group refrences from database like group invites for this group
+  await deleteDoc(groupRef);
+};
+
 export const getGroupRoles = (
   groupMembers: GroupMember[]
 ): GroupMemberRoles => {
@@ -67,4 +89,116 @@ export const getGroupRoles = (
     admins,
     members,
   };
+};
+
+export const sendGroupInvite = async (groupInvite: GroupInvite) => {
+  try {
+    await setDoc(
+      doc(
+        db,
+        'groupInvites',
+        `${groupInvite.groupId}_${groupInvite.recipientId}`
+      ),
+      groupInvite
+    );
+  } catch (err) {
+    throw new Error('Failed to send invite.');
+  }
+};
+
+export const fetchGroupInvitesByRecipient = async (
+  userId: FirebaseUserId
+): Promise<GroupInvite[]> => {
+  const q = query(
+    collection(db, 'groupInvites'),
+    where('recipientId', '==', userId),
+    where('status', '==', 'pending')
+  );
+  const snapshot = await getDocs(q);
+
+  if (snapshot.empty) return [];
+
+  return snapshot.docs.map((doc) => {
+    return { ...(doc.data() as GroupInvite) };
+  });
+};
+
+export const fetchGroupInvitesBySender = async (
+  senderId: FirebaseUserId,
+  groupId: GroupId
+): Promise<GroupInvite[]> => {
+  const q = query(
+    collection(db, 'groupInvites'),
+    where('senderId', '==', senderId),
+    where('groupId', '==', groupId),
+    where('status', '==', 'pending')
+  );
+  const snapshot = await getDocs(q);
+
+  if (snapshot.empty) return [];
+
+  return snapshot.docs.map((doc) => {
+    return { ...(doc.data() as GroupInvite) };
+  });
+};
+
+export const addGroupMember = async (
+  groupId: GroupId,
+  userObj: Pick<UserData, 'userId' | 'name' | 'avatarId'>
+) => {
+  const { userId, name, avatarId } = userObj;
+
+  const groupRef = doc(db, 'groups', groupId);
+  updateDoc(groupRef, {
+    memberIds: arrayUnion(userId),
+    members: arrayUnion({
+      userId,
+      name,
+      avatarId,
+      role: 'member',
+      joinedAt: Date.now(),
+    }),
+  });
+};
+
+export const removeGroupMember = async (
+  groupId: GroupId,
+  memberObj: GroupMember
+) => {
+  const { userId } = memberObj;
+
+  const groupRef = doc(db, 'groups', groupId);
+  await updateDoc(groupRef, {
+    memberIds: arrayRemove(userId),
+    members: arrayRemove({
+      ...memberObj,
+    }),
+  });
+};
+
+export const acceptGroupInvite = async (invite: GroupInvite) => {
+  const inviteRef = doc(
+    db,
+    'groupInvites',
+    `${invite.groupId}_${invite.recipientId}`
+  );
+  await updateDoc(inviteRef, {
+    status: 'accepted',
+  });
+};
+
+export const rejectGroupInvite = async (invite: GroupInvite) => {
+  const inviteRef = doc(
+    db,
+    'groupInvites',
+    `${invite.groupId}_${invite.recipientId}`
+  );
+  await updateDoc(inviteRef, {
+    status: 'rejected',
+  });
+};
+
+export const assignRole = (group: Group, userId: FirebaseUserId) => {
+  const member = group.members.find((mem) => mem.userId == userId);
+  return member ? member.role : 'spectator';
 };
--- src/pages/app/groups/[groupId].tsx
+++ src/pages/app/groups/[groupId].tsx
@@ -1,49 +1,176 @@
 import {
+  addGroupMember,
+  assignRole,
   fetchGroupById,
+  fetchGroupInvitesBySender,
+  FriendsInviteModal,
   GroupDetails,
   GroupMembersList,
+  removeGroupMember,
+  sendGroupInvite,
 } from '@/features/groups';
-import type { Group, GroupId } from '@/types';
+import { useUserData } from '@/features/user';
+import type { Friend, Group, GroupId, GroupInvite } from '@/types';
 import { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
 export default function GroupDetailsPage() {
+  const { userData } = useUserData();
   const location = useLocation();
   const { groupId } = useParams<{ groupId: GroupId }>();
   const { groupData } = location.state || {};
   const [group, setGroup] = useState<Group | null>(groupData || null);
+  const [showInviteModal, setShowInviteModal] = useState(false);
+  const [sentInvites, setSentInvites] = useState<GroupInvite[] | null>(null);
+  const [role, setRole] = useState<
+    'admin' | 'creator' | 'member' | 'spectator'
+  >('spectator');
 
   useEffect(() => {
-    if (group || !groupId) return;
+    if (!showInviteModal || !userData || !groupId || sentInvites) return;
+    fetchGroupInvitesBySender(userData.userId, groupId).then(setSentInvites);
+  }, [showInviteModal]);
 
-    fetchGroupById(groupId)
-      .then(setGroup)
-      .catch((err) => console.error(err));
-  }, [groupId]);
+  useEffect(() => {
+    if (!groupId || !userData) return;
+
+    if (!group) {
+      fetchGroupById(groupId)
+        .then(setGroup)
+        .catch((err) => console.error(err));
+    } else {
+      setRole(assignRole(group, userData.userId));
+    }
+  }, [groupId, userData, group]);
+
+  const inviteListFriends = (
+    invites: GroupInvite[] | null
+  ): (Friend & { invited: boolean })[] | null => {
+    if (!invites) return null;
+
+    const invitedUserIds = invites.map((invite) => invite.recipientId);
+    const friendsNotInGroup = userData!.friends.filter(
+      (friend) => !group?.memberIds.includes(friend.userId)
+    );
+    return friendsNotInGroup.map((friendObj) => {
+      return invitedUserIds.includes(friendObj.userId)
+        ? { ...friendObj, invited: true }
+        : { ...friendObj, invited: false };
+    });
+  };
+
+  function handleInvite(recipient: Friend & { invited: boolean }) {
+    if (!groupId || !group || !userData) return;
+    const {
+      userId: senderId,
+      name: senderName,
+      avatarId: senderAvatarId,
+    } = userData;
+    const { name: groupName, avatarId: groupAvatarId } = group;
+
+    const newInvite: GroupInvite = {
+      senderId,
+      senderName,
+      senderAvatarId,
+      groupId,
+      groupName,
+      groupAvatarId,
+      recipientId: recipient.userId,
+      createdAt: Date.now(),
+      status: 'pending',
+    };
+
+    sendGroupInvite(newInvite).catch((err) => console.error(err));
+    setSentInvites((prev) => [...prev!, newInvite]);
+  }
+
+  function handleJoin() {
+    if (!groupId || !userData) return;
+    const userObj = {
+      userId: userData.userId,
+      name: userData.name,
+      avatarId: userData.avatarId,
+    };
+
+    addGroupMember(groupId, userObj);
+
+    setRole('member');
+    setGroup((prev) => ({
+      ...prev!,
+      memberIds: [...prev!.memberIds, userObj.userId],
+      members: [
+        ...prev!.members,
+        { ...userObj, role: 'member', joinedAt: Date.now() },
+      ],
+    }));
+  }
+
+  function handleLeave() {
+    if (!groupId || !userData) return;
+    const memberObj = group?.members.find(
+      (mem) => mem.userId == userData.userId
+    );
+
+    removeGroupMember(groupId, memberObj!);
+
+    setRole('spectator');
+    const memberIds = group!.memberIds.filter((id) => id != userData.userId);
+    const members = group!.members.filter(
+      (mem) => mem.userId != userData.userId
+    );
+    setGroup((prev) => ({
+      ...prev!,
+      memberIds,
+      members,
+    }));
+  }
+
+  // TODO: add loading skeleton
+  if (!group || !userData) return <div>Loading...</div>;
 
   return (
     <main className="bg-white flex flex-col justify-between px-4 py-2">
-      {!group ? (
-        // TODO: add loading skeleton
-        <div>Loading...</div>
-      ) : (
-        <>
-          <GroupDetails group={group} />
-          <GroupMembersList members={group.members} />
-        </>
-      )}
+      <>
+        <GroupDetails group={group} />
+        <GroupMembersList members={group.members} />
+        <FriendsInviteModal
+          open={showInviteModal}
+          onOpenChange={setShowInviteModal}
+          onInvite={handleInvite}
+          friends={inviteListFriends(sentInvites)}
+        />
+        {/* Actions */}
+        <div className="actions-menu h-1/12 flex gap-4 mt-2">
+          {role != 'spectator' ? (
+            <>
+              {role != 'member' && (
+                <button
+                  onClick={() => setShowInviteModal(true)}
+                  className="flex-1 py-2 rounded-xl bg-blue-200 text-blue-700 font-semibold text-xl flex items-center justify-center gap-2 hover:bg-blue-300 transition-colors">
+                  <span className="text-2xl">&#8594;</span>
+                  Invite
+                </button>
+              )}
 
-      {/* Actions */}
-      <div className="actions-menu h-1/12 borde border-yellow-500 flex gap-4 mt-2">
-        <button className="flex-1 py-2 rounded-xl bg-blue-200 text-blue-700 font-semibold text-xl flex items-center justify-center gap-2 hover:bg-blue-300 transition-colors">
-          <span className="text-2xl">&#8594;</span>
-          Invite
-        </button>
-        <button className="flex-1 py-2 rounded-xl bg-red-100 text-red-400 font-semibold text-lg flex items-center justify-center gap-2 hover:bg-red-200 transition-colors">
-          <span className="text-2xl">&#8592;</span>
-          Leave
-        </button>
-      </div>
+              {role != 'creator' && (
+                <button
+                  onClick={handleLeave}
+                  className="flex-1 py-2 rounded-xl bg-red-100 text-red-400 font-semibold text-lg flex items-center justify-center gap-2 hover:bg-red-200 transition-colors">
+                  <span className="text-2xl">&#8592;</span>
+                  Leave
+                </button>
+              )}
+            </>
+          ) : (
+            <button
+              onClick={handleJoin}
+              className="flex-1 py-2 rounded-xl bg-blue-200 text-blue-700 font-semibold text-xl flex items-center justify-center gap-2 hover:bg-blue-300 transition-colors">
+              <span className="text-2xl">&#8594;</span>
+              Join
+            </button>
+          )}
+        </div>
+      </>
     </main>
   );
 }
