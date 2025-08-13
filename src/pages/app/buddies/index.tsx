c0765904b3ddbc6fcaed4ccbefba85a19f8ca66f
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-08-13 23:35:25 +0530

    feat: add RequestsList in intersection observer to fetch friend requests when needed

--- src/pages/app/buddies/index.tsx
+++ src/pages/app/buddies/index.tsx
@@ -1,16 +1,51 @@
 import SearchBox from '@/components/SearchBox';
 import SnapSections from '@/components/SnapSections';
-import { BuddiesList, RequestsList } from '@/features/buddies';
+import { useAuth } from '@/features/auth';
+import {
+  BuddiesList,
+  fetchRequestsByRecipient,
+  RequestsList,
+} from '@/features/buddies';
+import { useUserData } from '@/features/user';
+import type { FriendRequest, FirebaseUserId } from '@/types';
+import { useEffect, useRef, useState } from 'react';
 
 export default function BuddiesDashboard() {
+  const userId = useAuth().user?.uid as FirebaseUserId;
+  const { userData } = useUserData();
+  const requestListRef = useRef<HTMLDivElement | null>(null);
+  const [requests, setRequests] = useState<FriendRequest[] | null>(null);
+
+  useEffect(() => {
+    const section = requestListRef.current;
+    let observer: IntersectionObserver;
+
+    if (section && requests === null) {
+      observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            fetchRequestsByRecipient(userId).then(setRequests);
+            observer.disconnect();
+          }
+        },
+        { threshold: 0.5 }
+      );
+      observer.observe(section);
+    }
+
+    return () => observer && observer.disconnect();
+  }, [requests]);
+
   const sections = [
     {
       title: 'buddies',
       component: BuddiesList,
+      props: { friends: userData?.friends },
     },
     {
       title: 'requests',
       component: RequestsList,
+      props: { requests, setRequests, requestListRef },
     },
   ];
 
