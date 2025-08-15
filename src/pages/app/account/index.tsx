b3896f28decd0665f5c60a165b4e7e5cbaff655e
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-08-16 00:45:01 +0530

    feat: show user's self details on account page or by opening self profile from search

--- src/pages/app/account/index.tsx
+++ src/pages/app/account/index.tsx
@@ -1,31 +1,36 @@
-import ProfileStats from './ProfileStats';
-import ProfileDetails from './ProfileDetails';
+import { useAuth } from '@/features/auth';
+import {
+  FriendsAndJoined,
+  ProfileHeader,
+  StudyStats,
+  SubjectsAndStreak,
+  useUserData,
+} from '@/features/user';
 
 function AccountDashboard() {
+  const { userData } = useUserData();
+  const { logout } = useAuth();
+
   return (
-    <main className="flex flex-col gap-4 p-2">
-      <div className="flex-[2] flex flex-col gap-4 p-3 rounded-lg">
-        <h1 className="font-bold text-3xl">Profile</h1>
-        <ProfileDetails />
-        <ProfileStats />
-      </div>
-      <div className="flex-[3] flex flex-col gap-1 overflow-auto p-3 rounded-lg">
-        <h1 className="font-bold text-3xl">Settings</h1>
-        <div className="border border-black rounded-md px-1 py-2">Subjects</div>
-        <div className="border border-black rounded-md px-1 py-2">
-          Daily Target
-        </div>
-        <div className="border border-black rounded-md px-1 py-2">
-          Theme mode
-        </div>
-        <div className="border border-black rounded-md px-1 py-2">
-          Study time privacy
-        </div>
-        <div className="border border-black rounded-md px-1 py-2">
-          Change account password
-        </div>
-        <div className="border border-black rounded-md px-1 py-2">Logout</div>
-      </div>
+    <main className="bg-gray-100 grid  grid-rows-[2fr_1fr_1.2fr_1fr_0.6fr] gap-2 px-4 py-2">
+      {!userData ? (
+        // TODO: add loading skeleton
+        <div>Loading...</div>
+      ) : (
+        <>
+          <ProfileHeader userData={userData} />
+          <SubjectsAndStreak userData={userData} />
+          <StudyStats userData={userData} />
+          <FriendsAndJoined userData={userData} />
+
+          <button
+            className="flex-1 py-2 rounded-xl bg-red-200 text-red-700 font-semibold text-xl flex items-center justify-center gap-2 hover:bg-red-300 transition-colors"
+            // TODO: add confirmation dialog
+            onClick={logout}>
+            Logout
+          </button>
+        </>
+      )}
     </main>
   );
 }
