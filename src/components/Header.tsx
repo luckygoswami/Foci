fecfb0dd25191a0f1c58b81bc20c9f8ac085c454
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-09-08 16:25:37 +0530

    feat: Add Firebase Hosting GitHub Actions workflow. (closes #20) - update rtdb and firestore rules - show app version on hovering over the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 import { APP_NAME } from '@/constants/app';
+import meta from '@/../package.json';
 
 export function Header({ title = APP_NAME }: { title?: string }) {
   return (
-    <h1 className="w-full border-b-2 border-border/50 p-2 text-xl font-semibold text-foreground text-center">
+    <h1
+      title={`v${meta.version}`}
+      className="w-full border-b-2 border-border/50 p-2 text-xl font-semibold text-foreground text-center">
       {title}
     </h1>
   );
