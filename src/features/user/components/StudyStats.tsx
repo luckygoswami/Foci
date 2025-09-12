7d012faccd4fbaa706ceecf9c778c363ff66d252
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-09-13 00:32:32 +0530

    style: show well formatted detailed duration in user's profile

--- src/features/user/components/StudyStats.tsx
+++ src/features/user/components/StudyStats.tsx
@@ -2,6 +2,7 @@ import {
   fetchDailyGoalProgress,
   fetchWeeklyGoalProgress,
 } from '@/features/charts';
+import { formatDurationHM } from '@/lib/utils';
 import type { UserData } from '@/types';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
@@ -24,18 +25,20 @@ export function StudyStats({ userData }: { userData: UserData }) {
       <h3 className="font-semibold mb-2">Study Stats</h3>
       <div className="grid grid-cols-3 gap-2 text-center text-sm text-gray-600">
         <div className="border-r border-gray-300">
-          <p className="font-medium text-gray-800 text-lg">{dayProgress} m</p>
+          <p className="font-medium text-gray-800 text-lg">
+            {formatDurationHM(dayProgress)}
+          </p>
           <p>Today</p>
         </div>
         <div>
           <p className="font-medium text-gray-800 text-lg">
-            {Math.floor(weekProgress / 60)} h
+            {formatDurationHM(weekProgress)}
           </p>
           <p>This Week</p>
         </div>
         <div className="border-l border-gray-300">
           <p className="font-medium text-gray-800 text-lg">
-            {Math.floor(userData.totalStudyTime / 60)} h
+            {formatDurationHM(userData.totalStudyTime)}
           </p>
           <p>Total</p>
         </div>
