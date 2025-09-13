f02b3271539a3191d4d7753b10bbf7f81f9efe1c
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-09-13 17:25:32 +0530

    fix: use runTransaction while adding group member to improve atomicity

--- src/features/groups/services/groupMembers.ts
+++ src/features/groups/services/groupMembers.ts
@@ -1,35 +1,51 @@
-import { doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  runTransaction,
+} from 'firebase/firestore';
 import { db } from '@/lib/firebase-config';
-import type { GroupId, GroupMember, UserData } from '@/types';
+import type { Group, GroupId, GroupMember, UserData } from '@/types';
 
 export async function addGroupMember(
   groupId: GroupId,
   userObj: Pick<UserData, 'userId' | 'name' | 'avatarId'>
-) {
+): Promise<void> {
   const { userId, name, avatarId } = userObj;
-  const groupRef = doc(db, 'groups', groupId);
 
   try {
-    await updateDoc(groupRef, {
-      memberIds: arrayUnion(userId),
-      members: arrayUnion({
-        userId,
-        name,
-        avatarId,
-        role: 'member',
-        joinedAt: Date.now(),
-      }),
+    const groupRef = doc(db, 'groups', groupId);
+
+    await runTransaction(db, async (tx) => {
+      const groupSnap = await tx.get(groupRef);
+      const groupData = groupSnap.data() as Group;
+
+      if (groupData.memberIds.includes(userId)) {
+        throw new Error('Group already joined.');
+      }
+
+      tx.update(groupRef, {
+        memberIds: arrayUnion(userId),
+        members: arrayUnion({
+          userId,
+          name,
+          avatarId,
+          role: 'member',
+          joinedAt: Date.now(),
+        }),
+      });
     });
   } catch (err: any) {
     console.error('Error in adding group memeber:', err);
-    throw new Error('Unable to add member.');
+    throw new Error(err.message || 'Unable to add member');
   }
 }
 
 export async function removeGroupMember(
   groupId: GroupId,
   memberObj: GroupMember
-) {
+): Promise<void> {
   const { userId } = memberObj;
   const groupRef = doc(db, 'groups', groupId);
 
